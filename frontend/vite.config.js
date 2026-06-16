17917a9991fc36a33fd2d880feb08543bf21c3f0
jd-coderr <jedrabid@gmail.com>
2026-06-16 16:11:58 +0200

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,16 @@ export default defineConfig({
   plugins: [react()],
   preview: {
     allowedHosts: [
+      'www.iknowquantfu.com',
+      'iknowquantfu.com',
+      'www.bergmanntrading.com',
+      'bergmanntrading.com'
+    ]
+  },
+  server: {
+    allowedHosts: [
+      'www.iknowquantfu.com',
+      'iknowquantfu.com',
       'www.bergmanntrading.com',
       'bergmanntrading.com'
     ]
